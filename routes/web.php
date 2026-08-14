9d6a6feeff97520ba01364173ba2fa0c6435de07
Jesus Celis <geal16ster@gmail.com>
2026-08-14 14:59:56 -0400

    asd

--- routes/web.php
+++ routes/web.php
@@ -207,9 +207,11 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/detalles/{detalleId}/facturas', [PagoProveedorController::class, 'agregarFactura'])->name('facturas.agregar');
         Route::delete('/facturas/{facturaId}', [PagoProveedorController::class, 'eliminarFactura'])->name('facturas.eliminar');
 
-        Route::get('/reporte-facturas', [PagoProveedorController::class, 'reporteFacturas'])->name('reporte-facturas');
+
         Route::get('/exportar-facturas', [PagoProveedorController::class, 'exportarReporteFacturas'])->name('exportar-facturas');
     });
+
+    Route::get('/pagos-proveedores/reporte-facturas', [PagoProveedorController::class, 'reporteFacturas'])->name('reporte-facturas');
 
     Route::get('/motosporfecha', [PagoProveedorController::class, 'motosPorFecha'])->name('motosPorFecha');
 
